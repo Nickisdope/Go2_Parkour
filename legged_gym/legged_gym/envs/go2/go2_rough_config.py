aae5f50a94ebb739267e03a4e542b46c69fdc672
Nickisdope <liuxiangnick@gmail.com>
2025-10-20 22:46:44 +0800

    Initial clean commit

---
+++ legged_gym/legged_gym/envs/go2/go2_rough_config.py
@@ -0,0 +1,137 @@
+from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobotCfgPPO
+from matplotlib.pyplot import cla
+
+class Go2RoughCfg( LeggedRobotCfg ):
+    class env:
+        num_envs = 4096
+        num_observations = 294
+        num_privileged_obs = None # if not None a priviledge_obs_buf will be returned by step() (critic obs for assymetric training). None is returned otherwise 
+        num_actions = 12
+        env_spacing = 3.  # not used with heightfields/trimeshes 
+        send_timeouts = True # send time out information to the algorithm
+        episode_length_s = 20 # episode length in seconds
+
+    class terrain:
+        mesh_type = 'trimesh' # "heightfield" # none, plane, heightfield or trimesh
+        horizontal_scale = 0.05 # [m]
+        vertical_scale = 0.005 # [m]
+        border_size = 25 # [m]
+        curriculum = True
+        static_friction = 1.0
+        dynamic_friction = 1.0
+        restitution = 0.
+        # rough terrain only:
+        measure_heights = True
+        measured_points_x = [-0.3, -0.2, -0.1, 0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8] # 1mx1.6m rectangle (without center line)
+        measured_points_y = [-0.5, -0.4, -0.3, -0.2, -0.1, 0., 0.1, 0.2, 0.3, 0.4, 0.5]
+        selected = False # select a unique terrain type and pass all arguments
+        terrain_kwargs = None # Dict of arguments for selected terrain
+        max_init_terrain_level = 5 # starting curriculum state
+        terrain_length = 12.
+        terrain_width = 12.
+        num_rows= 10 # number of terrain rows (levels)
+        num_cols = 10 # number of terrain cols (types)
+        # terrain types: [smooth slope, rough slope, stairs up, stairs down, discrete, stepping, gap, pit]
+        # terrain_proportions = [0., 0., 0., 0., 0., 0., 1., 0.]
+        terrain_proportions = [0.1, 0.1, 0.2, 0.2, 0.2, 0.2, 0, 0.]
+        # trimesh only:
+        slope_treshold = 0.75 # slopes above this threshold will be corrected to vertical surfaces
+
+    class init_state( LeggedRobotCfg.init_state ):
+        pos = [0.0, 0.0, 0.42] # x,y,z [m]
+        default_joint_angles = { # = target angles [rad] when action = 0.0
+            'FL_hip_joint': 0.1,   # [rad]
+            'RL_hip_joint': 0.1,   # [rad]
+            'FR_hip_joint': -0.1 ,  # [rad]
+            'RR_hip_joint': -0.1,   # [rad]
+
+            'FL_thigh_joint': 0.8,     # [rad]
+            'RL_thigh_joint': 1.,   # [rad]
+            'FR_thigh_joint': 0.8,     # [rad]
+            'RR_thigh_joint': 1.,   # [rad]
+
+            'FL_calf_joint': -1.5,   # [rad]
+            'RL_calf_joint': -1.5,    # [rad]
+            'FR_calf_joint': -1.5,  # [rad]
+            'RR_calf_joint': -1.5,    # [rad]
+        }
+
+    class commands:
+        curriculum = False
+        max_curriculum = 1.
+        num_commands = 4 # default: lin_vel_x, lin_vel_y, ang_vel_yaw, heading (in heading mode ang_vel_yaw is recomputed from heading error)
+        resampling_time = 10. # time before command are changed[s]
+        heading_command = True # if true: compute ang vel command from heading error
+        class ranges:
+            lin_vel_x = [0., 1.5] # min max [m/s]
+            lin_vel_y = [-0.0, 0.0]   # min max [m/s]
+            ang_vel_yaw = [-1.2, 1.2]    # min max [rad/s]
+            heading = [-3.14, 3.14]
+
+    class control( LeggedRobotCfg.control ):
+        # PD Drive parameters:
+        control_type = 'P'
+        stiffness = {'joint': 20.}  # [N*m/rad]
+        damping = {'joint': 0.5}     # [N*m*s/rad]
+        # action scale: target angle = actionScale * action + defaultAngle
+        action_scale = 0.25
+        # decimation: Number of control action updates @ sim DT per policy DT
+        decimation = 4
+
+    class asset( LeggedRobotCfg.asset ):
+        file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/go2/urdf/go2.urdf'
+        name = "go2"
+        foot_name = "foot"
+        penalize_contacts_on = ["thigh", "calf"]
+        terminate_after_contacts_on = ["base", "Head_upper", "Head_lower"]
+        self_collisions = 1 # 1 to disable, 0 to enable...bitwise filter
+        flip_visual_attachments = True
+
+    class domain_rand:
+        randomize_friction = False
+        friction_range = [0.2, 1.5]
+        randomize_base_mass = False
+        added_mass_range = [-4., 4.]
+        push_robots = False
+        push_interval_s = 15
+        max_push_vel_xy = 1.
+
+        randomize_base_com = False
+        added_com_range = [-0.15, 0.15]
+
+        randomize_motor = False
+        motor_strength_range = [0.8, 1.2]
+
+    class rewards( LeggedRobotCfg.rewards ):
+        soft_dof_pos_limit = 0.9
+        base_height_target = 0.25
+        only_positive_rewards = True
+        class scales( LeggedRobotCfg.rewards.scales ):
+            torques = -0.0002
+            dof_pos_limits = -10.0
+
+class Go2SteppingCfg(Go2RoughCfg):
+    class terrain(Go2RoughCfg.terrain):
+        # terrain types: [smooth slope, rough slope, stairs up, stairs down, discrete, stepping, gap, pit]
+        terrain_proportions = [0., 0., 0., 0., 0., 1., 0,  0.]
+
+class Go2GapCfg(Go2RoughCfg):
+    class terrain(Go2RoughCfg.terrain):
+        # terrain types: [smooth slope, rough slope, stairs up, stairs down, discrete, stepping, gap, pit]
+        terrain_proportions = [0., 0., 0., 0., 0., 0., 1,  0.]
+
+class Go2PitCfg(Go2RoughCfg):
+    class terrain(Go2RoughCfg.terrain):
+        # terrain types: [smooth slope, rough slope, stairs up, stairs down, discrete, stepping, gap, pit]
+        terrain_proportions = [0., 0., 0., 0., 0., 0., 0,  1.]
+
+class Go2RoughCfgPPO( LeggedRobotCfgPPO ):
+    class algorithm( LeggedRobotCfgPPO.algorithm ):
+        entropy_coef = 0.01
+    class runner( LeggedRobotCfgPPO.runner ):
+        run_name = ''
+        experiment_name = 'rough_go2'
+        num_steps_per_env = 48 # per iteration
+        max_iterations = 15000
+
+  
